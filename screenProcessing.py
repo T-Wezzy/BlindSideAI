79452e4afdec660a35322489e258dee104a1d031
T-Wezzy <t.wezzy087@gmail.com>
2024-11-01 10:10:18 -0500

    Friday :D

--- screenProcessing.py
+++ screenProcessing.py
@@ -3,25 +3,50 @@ import cv2
 import easyocr
 import time
 import json
+import numpy as np
+
+from PIL import Image
+
+from matplotlib import pyplot as plt
 
 
 
 
 
-def screenshot(path, x, y, width, height):
+def screenshot(path):
 
-    pyautogui.screenshot(path, (x, y, width, height))
+    pyautogui.screenshot(path)
     print("Stored screenshot to: " + str(path) + " Successfully!")
 
-def textFromImage(imgPath):
-    img = cv2.imread(imgPath)
+def textFromImage(startRow, startColumn, width, height, debugMode):
+
+    img = cv2.imread(r'C:\\Users\\ace\\Music\\BlindSideAI-Trevin\\BlindSideAI-py\\screenshots\\fullscreenShot.jpg')
+    img = cv2.resize(img, (1920, 1080))
+    if debugMode:
+        cv2.imshow("original", img)
+    
+    # Cropping an image
+    cropped_image = img[startColumn:height, startRow:width]
+    
+    if debugMode:
+        # Display cropped image
+        cv2.imshow("cropped", cropped_image)
+    
+    # Save the cropped image
+    cv2.imwrite("screenshots\\Cropped Image.jpg", cropped_image)
+
+    if debugMode:
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
     reader = easyocr.Reader(['en'], gpu=False)
 
-    text = reader.readtext(img)
+    text = reader.readtext(cropped_image)
 
     for i in text:
-        print(i)
+        # print(i)
         boundBox, text_, score = i
+        print("Text: " + text_ + " Probability: " + str(score))
         return text_
 
 def updateJSON():
@@ -41,25 +66,25 @@ def updateJSON():
 
 # time.sleep(3)
 
-# screenshot("screenshots\\bossShot.png", 75, 75, 425, 75)
+screenshot("screenshots\\fullscreenShot.jpg")
 
 # screenshot("screenshots\\chipsShot.png", 236, 232, 235, 54)
 
 # screenshot("screenshots\\handShot.png", 1000, 862, 90, 27)
 
-blind = textFromImage("screenshots\\bossShot.png")
+blind = textFromImage(82, 80, 490, 145, True)
 
-chips = str(textFromImage("screenshots\\chipsShot.png"))
+chips = str(textFromImage())
 
-handData = str(textFromImage("screenshots\\handShot.png"))
+handData = str(textFromImage())
 handData = handData.split("/")
 
-print(handData)
+#print(handData)
 
-if chips.find(','):
-    chips = chips.replace(',', '')
+# if chips.find(','):
+#     chips = chips.replace(',', '')
 
-updateJSON()
+# updateJSON()
 
 
 
